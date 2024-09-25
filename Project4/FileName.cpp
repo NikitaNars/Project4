01c1fdb4fc6d48372cfd18b618e23f19f8f15836
Nikita Narseev <nne150307@gmail.com>
2024-09-25 15:15:58 +0500

    Добавьте файлы проекта.

---
+++ Project4/FileName.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+	int num, evenNum = 0, oddNum = 0;
+
+
+	do
+	{
+		std::cout << "num = ";
+		std::cin >> num;
+		if (num % 2 == 0)
+		{
+			evenNum += num;
+		}
+		else
+		{
+			oddNum += num;
+		}
+	} while (num != 0);
+	
+	std::cout << "Even: " << evenNum << "\nOdd: " << oddNum;
+
+	return 0;
+}
